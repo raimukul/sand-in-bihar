5710427258c2fe7a4edbac73ad54472b38e8fb73
Mukul Rai <47410631+raimukul@users.noreply.github.com>
2021-04-29 22:39:32 +0530

    repo added

---
+++ src/Components/Index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header/header'
+import Footer from './Footer/footer'
+
+function Index() {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default Index
