a4b47138bb5adc8fca675968637943dc2e0faf4b
Mukul Rai <47410631+raimukul@users.noreply.github.com>
2021-04-29 23:09:30 +0530

    carousel added

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header/header'
 import Footer from './Footer/footer'
+import Carousel from './Body/carousel'
 
 function Index() {
     return (
         <div>
             <Header />
+            <Carousel />
             <Footer />
         </div>
     )
